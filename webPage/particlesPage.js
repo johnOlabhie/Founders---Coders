bbea446e25223c48610b58e89eda7e8a23e2eb81
johnOlabhie <89073227+johnOlabhie@users.noreply.github.com>
2024-01-24 18:26:05 +0000

    added new pages, parteclies

---
+++ webPage/particlesPage.js
@@ -0,0 +1,10 @@
+particlesJS("particles-container", { //id of container
+    particles: {
+    number: { value: 50,} ,
+    color: { value:  "#ecddb0", } ,
+    shape: {  type: "star",  },
+     size: { value: 7, },
+    move: { enable: true, speed: 6, },
+
+    },
+});
---
+++ webPage/playSound.js
@@ -0,0 +1,5 @@
+function playSound(audioId) {
+    const audio = document.getElementById(audioId);
+    audio.play();
+}
+
