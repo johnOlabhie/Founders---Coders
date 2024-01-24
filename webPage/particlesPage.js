70bca239b192086c7a25fcf64465b9dd5802b9ca
johnOlabhie <89073227+johnOlabhie@users.noreply.github.com>
2024-01-24 23:29:05 +0000

    added slide show, and more particles

--- webPage/particlesPage.js
+++ webPage/particlesPage.js
@@ -1,10 +1,11 @@
 particlesJS("particles-container", { //id of container
     particles: {
     number: { value: 50,} ,
-    color: { value:  "#ecddb0", } ,
+    color: { value:  "#F1C232", } ,
     shape: {  type: "star",  },
      size: { value: 7, },
     move: { enable: true, speed: 6, },
-
+    
     },
 });
+
